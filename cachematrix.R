14f8826cfe94cd48fba20f9c364e3a49c82ecd21
Markus Wilhelm <markus.wilhelm91@me.com>
2014-12-20 18:11:05 +0100

    updated code with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+# The makeCacheMatrix functions creates a special matrix, which saves a normal matrix and the inverse of that matrix.
+# The function return a list with the getters and setters for the matrix and inverse.
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
         
@@ -18,7 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
              getInverse = getInverse)
 }
 
-
+# The cacheSolve function creates the inverse of the matrix saved in the makeCacheMatrix function.
+# The function returns the saved inverse, if there is already one, or returns the newly created one.
 cacheSolve <- function(x, ...) {
         i <- x$getInverse()
         
@@ -29,6 +32,7 @@ cacheSolve <- function(x, ...) {
         
         data <- x$get()
         
+        # solve() creates the inverse of a given matrix
         i <- solve(data)
         
         x$setInverse(i)
